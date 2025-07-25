2a6823c3ff6ad74d18b95e401e6f99b027ad8070
Hemiit <hemiit@foxmail.com>
2025-07-25 13:00:59 +0800

    Feat: SmallBall_FlashingVFX

--- Assets/Scripts/Scene/SmallBall.cs
+++ Assets/Scripts/Scene/SmallBall.cs
@@ -1,18 +1,21 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SmallBall : MonoBehaviour
 {
     public Vector2 position;    //Input on the Unity_Inspector_pnl.
     public GameObject pnlImg;
-
+    public Transform flashingVFX;
 
     // Start is called before the first frame update
     void Start()
     {
         SetInitPos();
         HidePnlImg();
+        AnimFlashing();
     }
     private void SetInitPos()
     {
@@ -21,12 +24,28 @@ public class SmallBall : MonoBehaviour
     public void Show() { this.gameObject.SetActive(true); }
     public void Hide() { this.gameObject.SetActive(false); }
 
-    public void ShowPnlImg()
-    { 
-        pnlImg.SetActive(true);
+    public void AnimFlashing()
+    {
+        flashingVFX.localScale = new Vector3(0.5f, 0.5f, 0.5f);
 
+        //DOTween.Sequence()
+        //    .Append(Img_flashing.DOScale(1f, 1f))
+        //    .Append(Img_flashing.DOScale(0.5f, 1f))
+        //    .SetLoops(-1, LoopType.Restart);
+
+        var imgFlashing=flashingVFX.GetComponent<SpriteRenderer>();
+            
+        DOTween.Sequence()
+            .Append(flashingVFX.DOScale(1.5f, 1.5f))
+            .Join(imgFlashing.DOFade(0f,1f))
+            .SetLoops(-1, LoopType.Restart);
     }
-    public void HidePnlImg() 
+
+    public void ShowPnlImg()
+    {
+        pnlImg.SetActive(true);
+    }
+    public void HidePnlImg()
     {
         pnlImg.SetActive(false);
     }
